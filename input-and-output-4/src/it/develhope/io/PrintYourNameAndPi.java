4a22c9e433f460d265bb296a9aaac77a941e33a9
SvalioZZ <103143646+SvalioZZ@users.noreply.github.com>
2022-09-28 14:17:47 +0200

    Add files via upload

---
+++ input-and-output-4/src/it/develhope/io/PrintYourNameAndPi.java
@@ -0,0 +1,10 @@
+package it.develhope.io;
+
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        String name = "valerio";
+        double pi = 3.141590;
+        System.out.printf("your name is %S and Pi is %f", name, pi);
+    }
+    
+}
