d58e427909728dfd37e11ee88c99312f9e7c122c
SvalioZZ <103143646+SvalioZZ@users.noreply.github.com>
2022-09-28 14:25:13 +0200

    Add files via upload

---
+++ input-and-output-5/src/it/develhope/io/PrintYourAgeFromInput.java
@@ -0,0 +1,14 @@
+package it.develhope.io;
+
+import java.util.Scanner;
+
+public class PrintYourAgeFromInput {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your age: ");
+    int age = scanner.nextInt();
+    System.out.printf("Your age is %d", age);
+    }
+
+   
+}
