0fb75b0c0d92c32152a629e29cb64a1dd9ba69b0
SvalioZZ <103143646+SvalioZZ@users.noreply.github.com>
2022-09-30 17:08:53 +0200

    Add files via upload

---
+++ constructor-1/src/it/develhope/constructor/House.java
@@ -0,0 +1,26 @@
+package it.develhope.constructor;
+
+/**
+ * This class describes a house.
+ * @author Mr Wolf
+ */
+    public class House {
+
+       public String address;
+       public int numbersOfFloor;
+
+       public House(String address, int numbersOfFloor){
+           System.out.println("------ New House Initialised ------" +
+                   "\n--------------------" + "\n" + address + "\n---------------------" +
+                   "\n" + numbersOfFloor);
+       }
+}
+
+    // instance variables
+   
+
+    /**
+     * Constructor method: creates a House object and prints a message in console
+     */
+    
+
---
+++ constructor-1/src/it/develhope/constructor/Start.java
@@ -0,0 +1,29 @@
+package it.develhope.constructor;
+
+/**
+ * Tester class with main method.
+ * @author Mr Wolf
+ */
+public class Start {
+    /**
+     * Main method from where the Java program starts.
+     * @param args for all the parameters from command line
+     */
+    public static void main(String[] args) {
+        House house1, house2;
+        house1 = new House("via Roma , 11", 4 );
+        house2 = new House("via Padova, 10", 2);
+    }
+}
+
+
+
+                  // two variables declaration
+
+               // first House object creation and variable assignment
+
+                 // second House object creation and variable assignment
+
+    
+
+
